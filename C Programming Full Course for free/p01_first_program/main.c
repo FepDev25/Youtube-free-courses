08f51fb64efc0c85739fe42d215ec9a369d606f3
Felipe <lamediatricolor@gmail.com>
2025-10-20 00:55:42 -0500

    Iniciando curso C

---
+++ C Programming Full Course for free/p01_first_program/main.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main() {
+
+  // Este es un comentario en C
+  // Este es mi primer programa en C
+  printf("Hola mundo desde C!\n");
+  printf("Me gusta el futbol\n");
+
+  printf("Prueba desde neovim!\n");
+
+  return 0;
+}
